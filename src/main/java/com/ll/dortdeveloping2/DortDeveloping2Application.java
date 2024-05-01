b925e522582892b6b898b4c304937ce1311409cf
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-05-01 15:09:40 +0900

    settings

---
+++ src/main/java/com/ll/dortdeveloping2/DortDeveloping2Application.java
@@ -0,0 +1,13 @@
+package com.ll.dortdeveloping2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DortDeveloping2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DortDeveloping2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/dortdeveloping2/DortDeveloping2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.dortdeveloping2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DortDeveloping2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
