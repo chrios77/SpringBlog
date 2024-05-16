6e0bbce980608642e2a714b67c2ddde435000064
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-05-16 17:49:15 +0900

    Add Dto (AddArticleRequest)

---
+++ src/main/java/com/ll/dortdeveloping2/springbootdeveloper/dto/AddArticleRequest.java
@@ -0,0 +1,23 @@
+package com.ll.dortdeveloping2.springbootdeveloper.dto;
+
+import com.ll.dortdeveloping2.springbootdeveloper.domain.Article;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class AddArticleRequest {
+
+    private String title;
+
+    private String content;
+
+    public Article toEntity(){
+        return Article.builder()
+                .title(title)
+                .content(content)
+                .build();
+    }
+}
