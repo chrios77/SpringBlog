c3b8b0b0e8c6f5adae91a229ae14f89c89e566c2
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-05-29 13:56:40 +0900

    modify Article in domain package

--- src/main/java/com/ll/dortdeveloping2/springbootdeveloper/domain/Article.java
+++ src/main/java/com/ll/dortdeveloping2/springbootdeveloper/domain/Article.java
@@ -2,7 +2,13 @@ package com.ll.dortdeveloping2.springbootdeveloper.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
 @Entity
 @Getter
 @AllArgsConstructor
@@ -18,6 +24,14 @@ public class Article {
 
     @Column(name = "content", nullable = false)
     private String content;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
     @Builder
     public Article(String title, String content){
