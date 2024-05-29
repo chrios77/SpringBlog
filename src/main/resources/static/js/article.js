11093d6ebe637b86605516736a1ef6139d0e332f
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-05-29 21:22:29 +0900

    Add modifyBButton in article.js

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -12,3 +12,27 @@ if(deleteButton){
             });
     });
 }
+
+const modifyButton = document.getgetElementById('modify-btn');
+
+if(modifyButton){
+    modifyButton.addEventListener('click', event => {
+        let params = new URLSearchParams(location.search);
+        let id = params.get('id');
+
+        fetch(`/api/articles/${id}` , {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    title: document.getElementByID('title').value,
+                    content: document.getElementById('content').value
+                })
+            })
+            .then(() => {
+               alert('수정이 완료되었습니다.');
+               location.replace(`/articles/${id}`);
+            });
+        });
+}
