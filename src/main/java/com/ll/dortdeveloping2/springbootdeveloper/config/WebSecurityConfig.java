55d68cf258fc9b51ba11ebf9447b04ccdae9c522
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-07-10 14:28:34 +0900

    Resolve Problem -> Add BCryptPasswordEncoder

--- src/main/java/com/ll/dortdeveloping2/springbootdeveloper/config/WebSecurityConfig.java
+++ src/main/java/com/ll/dortdeveloping2/springbootdeveloper/config/WebSecurityConfig.java
@@ -1,17 +1,14 @@
 package com.ll.dortdeveloping2.springbootdeveloper.config;
 
 import com.ll.dortdeveloping2.springbootdeveloper.service.UserDetailService;
-
 import lombok.RequiredArgsConstructor;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -53,5 +50,10 @@ public class WebSecurityConfig {
                 )
                 .csrf(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
