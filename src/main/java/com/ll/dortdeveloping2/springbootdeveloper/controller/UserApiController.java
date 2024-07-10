9c8a9b31495e333594a76e26e74a9d6a4427bbcf
JeUk Park <77144006+sleo113@users.noreply.github.com>
2024-07-10 13:42:00 +0900

    Add method in UserApi Controller

--- src/main/java/com/ll/dortdeveloping2/springbootdeveloper/controller/UserApiController.java
+++ src/main/java/com/ll/dortdeveloping2/springbootdeveloper/controller/UserApiController.java
@@ -2,8 +2,13 @@ package com.ll.dortdeveloping2.springbootdeveloper.controller;
 
 import com.ll.dortdeveloping2.springbootdeveloper.dto.AddUserRequest;
 import com.ll.dortdeveloping2.springbootdeveloper.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @RequiredArgsConstructor
@@ -16,5 +21,12 @@ public class UserApiController {
     public String signup(AddUserRequest request){
         userService.save(request); //회원가입 메서드 호출
         return "redirect:/login"; // 회원강비 완료 후 로그인 페이지로 이동
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response){
+        new SecurityContextLogoutHandler().logout(request, response,
+                SecurityContextHolder.getContext().getAuthentication());
+        return "redirect:/login";
     }
 }
